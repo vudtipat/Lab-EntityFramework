c00216c6cb6d930e733272a3cdfec14271bc7a58
vudtipat <vudtipat.saishana@krungsrinimble.com>
2024-05-04 13:34:16 +0700

    Add Product on Pizza db

--- EFCore-Lab/Program.cs
+++ EFCore-Lab/Program.cs
@@ -1,4 +1,7 @@
-﻿class Program
+﻿using EFCore_Lab.Data;
+using EFCore_Lab.Models;
+
+class Program
 {
     static void Main(string[] args)
     {
@@ -10,5 +13,32 @@
         // if we have update model or context we must run command `dotnet ef migrations add MessageToLog` and use `dotnet ef database update` to update database
 
         Console.WriteLine("Hello, world!");
+
+        #region Add Product Section
+
+        // we use using because it ensure that PizzaContext it will be disposed off when done use it.
+        using PizzaContext pizzaContext = new PizzaContext();
+
+        Product hawaiiantPizza = new Product()
+        {
+            Name = "Deluxe Hawaiint Pizza1",
+            Price = 399M
+        };
+
+        Product deluxeMeatPizza = new Product()
+        {
+            Name = "Deluxe Meat Pizza1",
+            Price = 439M
+        };
+
+        pizzaContext.Add(hawaiiantPizza);
+        pizzaContext.Products.Add(deluxeMeatPizza);
+
+        // use SaveChanges() to persist the change to the database
+        pizzaContext.SaveChanges();
+
+        Console.WriteLine("Done!");
+
+        #endregion
     }
 }
